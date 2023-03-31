2181f1ffbb0dfa1c9a61eef39095f6e89f61d575
leejaejoseph <leejaejoseph@gmail.com>
2023-03-30 18:48:48 -0700

    Created animated logo from utilizing After-Effects.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+
 function App() {
   
   return (
-    <div>
-      3D Portfolio Page
-    </div>
+    <BrowserRouter>
+      <div classname="relative z-0 bg-primary">
+        <div bg-hero-pattern bg-cover bg-nop-repeat bg-center>
+          <Navbar/>
+          <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
