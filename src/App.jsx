e98db8eee9d7cce1908070e50b75f57f0d98adb0
leejaejoseph <leejaejoseph@gmail.com>
2023-04-10 00:06:27 -0700

    reformatted camera work for three.js.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef,   } from 'react';
 import * as THREE from 'three';
 import { BrowserRouter } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
@@ -26,62 +26,21 @@ function Plane(props) {
   return <primitive object={gltf.scene} {...props} />;
 }
 
-function MyComponent() {
-  const { camera } = useThree();
-  console.log(camera.position);
-  const orbitRef = useRef();
-  useFrame(() => {
-    // console.log(orbitRef.current.target);
-    // const azimuthalAngle = orbitRef.current.getAzimuthalAngle();
-    // const polarAngle = orbitRef.current.getPolarAngle();
-    // const zoom = orbitRef.current.zoom;
-
-    // console.log(`Azimuthal angle: ${azimuthalAngle}, Polar angle: ${polarAngle}, Zoom: ${zoom}`);
-  });
-  return <OrbitControls rotate={[0, Math.PI / 2, 0]} />;
-}
-
-function CameraPosition() {
-  const { camera } = useThree();
-  const orbitRef = useRef();
-  const [azimuthal, setAzimuthal] = useState(0);
-  const [polar, setPolar] = useState(0);
-
-  useFrame(() => {
-    if (orbitRef.current) {
-      const azimuthalAngle = orbitRef.current.getAzimuthalAngle();
-      const polarAngle = orbitRef.current.getPolarAngle();
-
-      setAzimuthal(azimuthalAngle);
-      setPolar(polarAngle);
-    }
-  });
-
-  return (
-    <div style={{ position: "absolute", top: 10, left: 10 }}>
-      <div>Azimuthal angle: {azimuthal.toFixed(2)}</div>
-      <div>Polar angle: {polar.toFixed(2)}</div>
-    </div>
-  );
-}
-
 export default function App() { 
   return (
     <BrowserRouter>
       <Navbar/>
-      <Canvas camera={{ position: [0, 5, 10], rotation: [0, 0, 0] }}>
-      <color attach="background" args={[0xf5deb3]} />
-      <PerspectiveCamera makeDefault={true} position={[0, 0, 100]} />
-      <ambientLight intensity={0.5}/>
-      <spotLight position={[0, 0, 0]} angle={0.3} />
-      <Torus position={[-5, 0, -10]}/>
-      <LinkedIn position={[0, 0, 0]} />
-      <Resume position={[0, 0, 0]}/>
-      <Plane position={[0, 0, 0]} />
-      <Physics/>
-      <OrbitControls ref={orbitRef} rotate={[0, Math.PI / 2, 0]} />
-      <CameraPosition />
-    </Canvas>
+      <Canvas camera={{ position: [0, 20, 20], rotation: [Math.PI/4, 0, 0]}}>
+        <OrbitControls makeDefault/>
+        <color attach="background" args={[0xf5deb3]} />
+        <ambientLight intensity={0.5}/>
+        <spotLight position={[0, 0, 0]} angle={0.3} />
+        <Torus position={[-5, 0, -10]}/>
+        <LinkedIn position={[0, 0, 0]} />
+        <Resume position={[0, 0, 0]}/>
+        <Plane position={[0, 0, 0]} />
+        <Physics/>
+      </Canvas>
     </BrowserRouter>
   )
 }
