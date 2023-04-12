73e55137097e0839f525e70a82ac2cd73707665a
leejaejoseph <leejaejoseph@gmail.com>
2023-04-11 18:03:59 -0700

    Added postgresql and react model.

--- src/App.jsx
+++ src/App.jsx
@@ -32,19 +32,31 @@ function CubeNetwork(props) {
   return <primitive object={gltf.scene} {...props} />;
 }
 
+function React(props) {
+  const gltf = useGLTF('/src/assets/Portfolio-blender/react.gltf');
+  return <primitive object={gltf.scene} {...props} />;
+}
+
+function Postgresql(props) {
+  const gltf = useGLTF('/src/assets/Portfolio-blender/postgresql.gltf');
+  return <primitive object={gltf.scene} {...props} />;
+}
+
+
 export default function App() { 
   return (
     <BrowserRouter>
       <Navbar/>
-      <Canvas styles={{ height: 400, width: 400 }}>
+      <Canvas shadow="true" styles={{ height: 400, width: 400 }}>
+        <hemisphereLight color="white" groundColor="blue" intensity={0.75} />
         <color attach="background" args={[0x000000]}/>
-        <ambientLight intensity={1}/>
-        <spotLight position={[0, 0, 0]} angle={0.3}/>
         <Torus position={[-5, -5, -10]}/>
         <LinkedIn position={[0, -5, 0]}/>
         <Resume position={[0, -5, 0]}/>
         <Plane position={[0, -10, 0]}/>
         <CubeNetwork position={[0, -10, 0]}/>
+        <React position={[-40, -10, 0]} rotation={[0, -Math.PI / 4, 0]} />
+        <Postgresql position={[-40, 0, 0]}/>
         <OrbitControls
           enableRotate={false}
         />
