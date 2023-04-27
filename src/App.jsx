580d99c91b8c4f7767e2024366ecfbcf0b98d8ee
leejaejoseph <leejaejoseph@gmail.com>
2023-04-27 00:43:52 -0700

    In-progress, allow click to change camera controls on state.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { Resume } from './components/Resume'
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { BeTabd } from './components/BeTabd'
 import { EsportsViewer } from './components/EsportsViewer'
-import { OrbitControls, OrthographicCamera, Environment, useGLTF, useHelper, Loader } from '@react-three/drei';
+import { OrbitControls, OrthographicCamera, Environment, useGLTF, useHelper, Loader, PerspectiveCamera } from '@react-three/drei';
 import { useControls } from 'leva';
 import { BLight } from './components/BackLights';
 import { Reactor } from './components/Reactor';
@@ -22,6 +22,11 @@ import { Overlay } from './components/Overlay';
 
 
 export default function App() {
+  const orthographicCameraRef = useRef();
+  const perspectiveCameraRef = useRef();
+
+  const [isOrthographic, setIsOrthographic] = useState(true);
+
   const cameraCont = useControls({z:20, n:1, f:2000, x2:160, y2:151, z2:240, li:1, l1:1, l2:2, l3:3})
   return (
     <BrowserRouter>
@@ -30,6 +35,8 @@ export default function App() {
       <Canvas 
       style={
         {backgroundImage: 'linear-gradient(to bottom, #000000 50%, #33a6ff)'}}
+        camera={isOrthographic ? orthographicCameraRef.current : perspectiveCameraRef.current}
+      
         >
         <Environment background preset="night"/>
         <BLight/>
@@ -41,20 +48,24 @@ export default function App() {
         <Resume/>
         <BeTabd/>
         <Reactor/>
-        <AboutMe/>
+        <AboutMe onClose={setIsOrthographic(!isOrthographic)}/>
         <EsportsViewer/>
         <Filler/>
         <OrbitControls
           enablePan={true}
           enableRotate={false}
-        />
+          />
         <OrthographicCamera
           makeDefault
+          ref={orthographicCameraRef}
           zoom={cameraCont.z}//{25}
           near={cameraCont.n}//1
           far={cameraCont.f}//{2000}
           position={[cameraCont.x2, cameraCont.y2, cameraCont.z2]}//[180, 120, 200]}
         />
+        <PerspectiveCamera
+          ref={perspectiveCameraRef}
+          position={[0, 140, 200 ]}/>
         <Plane/>
       </Canvas>
     </BrowserRouter>
--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,31 +2,30 @@ import React, { useContext, useRef, useEffect, useState } from 'react';
 import { useGLTF, Html, OrbitControls } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { useControls } from 'leva';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-export function AboutMe(props) {
+export function AboutMe(props, onClose) {
   const [showModal, setShowModal] = useState(false);
-  const cameraRef = useRef();
   const aboutMe = useGLTF('/src/assets/Portfolio-blender/aboutme.glb');
   const pos = useControls({xa: 0, ya: 0, za:0})
   const navigate = useNavigate();
-  useFrame((state) => {
-    // console.log(state.camera)
-    if (showModal) {
-      state.camera.position.lerp({ x: 0, y: 140, z: 200 }, 0.03)
-      state.camera.lookAt(42, 0, 50)
-      state.camera.updateProjectionMatrix()
+  const location = useLocation();
 
-    } 
-    else {
-      // state.camera.position.lerp({x: 180, y: 120, z: 200}, 0.03); // Reset camera position
-      // state.camera.lookAt(0, 0, 0);
-      // state.camera.updateProjectionMatrix()
+  // useFrame((state) => {
+  //   if (showModal) {
+  //     state.camera.position.lerp({ x: 0, y: 140, z: 200 }, 0.03)
+  //     state.camera.lookAt(42, 0, 50)
+  //     state.camera.updateProjectionMatrix()
+  //   }
+  // })
+
+  useEffect(()=> {
+    if (location.pathname === '/') {
+      setShowModal(false);
     }
-  })
-  
+}, [location])
+
   return (
   <>
-    <primitive ref={cameraRef} position={[42, 0, 50]}  onClick={(e)=>{e.stopPropagation(); setShowModal(!showModal); navigate('/about')}} object={aboutMe.scene}{...props}/>
-    <OrbitControls enabled={showModal} enableRotate={false} />
+    <primitive position={[42, 0, 50]}  onClick={(e)=>{e.stopPropagation(); setShowModal(!showModal); navigate('/about');  onClose();}} object={aboutMe.scene}{...props}/>
   </>)}
