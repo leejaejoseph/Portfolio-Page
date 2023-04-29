cad76becea742b39aa9eb441da46051c1064342c
leejaejoseph <leejaejoseph@gmail.com>
2023-04-28 23:42:52 -0700

    deployment .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import copy from 'rollup-plugin-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // ...
+  build: {
+    rollupOptions: {
+      plugins: [
+        copy({
+          targets: [
+            { src: 'src/assets/reactor.glb', dest: 'dist' }
+          ]
+        })
+      ]
+    }
+  }
+});
