1c156434000ea19f92546de235df8cb90cf68a61
leejaejoseph <leejaejoseph@gmail.com>
2023-04-29 01:31:22 -0700

    rev .

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,4 @@
-import { defineConfig } from 'vite';
-import copy from 'rollup-plugin-copy';
-
-export default defineConfig({
-  // ...
-  build: {
-    rollupOptions: {
-      plugins: [
-        copy({
-          targets: [
-            { src: 'src/assets/reactor.glb', dest: 'dist' }
-          ]
-        })
-      ]
-    }
-  }
-});
+// vite.config.js
+export default {
+  // config options
+}
